1ae076abfc646a011ec87ecd6cd2148755b53f23
Matthew Wakeham <mwakeham95@gmail.com>
2024-08-09 21:03:04 +0000

    Logging

--- app/main.go
+++ app/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"embed"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
+//go:embed index.html
+var content embed.FS
 
-func main () {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := content.ReadFile("index.html")
+		if err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(data)
+	})
+
+	log.Println("Server is running on :8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
